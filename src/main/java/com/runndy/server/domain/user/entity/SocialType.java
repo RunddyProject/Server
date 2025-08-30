0e22d2159e341c9746a5b7a22d1a5d9bac8bff51
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2025-08-30 16:23:27 +0900

    [CHORE]: SocialType Label 추가 #3

--- src/main/java/com/runndy/server/domain/user/entity/SocialType.java
+++ src/main/java/com/runndy/server/domain/user/entity/SocialType.java
@@ -1,7 +1,13 @@
 package com.runndy.server.domain.user.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SocialType {
-  GOOGLE,
-  NAVER,
-  KAKAO
+  NAVER("네이버"),
+  KAKAO("카카오");
+
+  private final String label;
 }

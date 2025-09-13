68cf76495959ad19e10735421b021bb7311e6ed5
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2025-09-13 22:52:12 +0900

    [FIX]: local - cors

--- src/main/java/com/runndy/server/config/CorsConfig.java
+++ src/main/java/com/runndy/server/config/CorsConfig.java
@@ -1,6 +1,11 @@
 package com.runndy.server.config;
 
+import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +18,23 @@ public class CorsConfig implements WebMvcConfigurer {
             .allowedOriginPatterns("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD") // 허용할 HTTP method
             .allowCredentials(true); // 쿠키 인증 요청 허용
+  }
+
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(
+        List.of("http://localhost:3000",
+            "https://localhost:3000",
+            "https://www.runddy.co.kr",
+            "https://api.runddy.co.kr"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }

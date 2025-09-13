e56c513a1a489efda1d6685d7fc47bb97cc0cca6
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2025-09-13 14:13:28 +0900

    [FEAT]: CORS

--- src/main/java/com/runndy/server/config/CorsConfig.java
+++ src/main/java/com/runndy/server/config/CorsConfig.java
@@ -1,5 +1,17 @@
 package com.runndy.server.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD") // 허용할 HTTP method
+            .allowCredentials(true); // 쿠키 인증 요청 허용
+  }
 }
